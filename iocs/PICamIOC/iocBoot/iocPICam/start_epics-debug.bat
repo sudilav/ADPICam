adc176b801996683afa8b8245078a2f00d772b32
John Hammond <JPHammonds@anl.gov>
2015-01-21 14:00:51 -0600

    Change base directory in scripts to C:\PICAM

--- iocs/PICamIOC/iocBoot/iocPICam/start_epics-debug.bat
+++ iocs/PICamIOC/iocBoot/iocPICam/start_epics-debug.bat
@@ -1,7 +1,3 @@
-REM set AREA_DETECTOR=%SUPPORT%\areaDetector_2_git
-REM set EPICS_DISPLAY_PATH=%AREA_DETECTOR%\ADPICam\PICamApp\op\adl;%AREA_DETECTOR%ADCore\ADApp\op\adl
-REM echo %EPICS_DISPLAY_PATH%
-REM start medm -x -macro "P=13PICAM1:, R=cam1:" PICam.adl
-REM start medm -x -macro "P=13PICAM1:, R=cam1:" ADBase.adl
+REM Start script for PICAM IOC
 ..\..\bin\windows-x64-debug\PICamApp st.cmd
 
--- iocs/PICamIOC/iocBoot/iocPICam/start_epics-displays.bat
+++ iocs/PICamIOC/iocBoot/iocPICam/start_epics-displays.bat
@@ -1,6 +1,6 @@
-set SUPPORT=C:\jhSrc\synApps_5_7\support
+set SUPPORT=C:\PICAM\synApps_5_7\support
 set AREA_DETECTOR=%SUPPORT%\areaDetector_2_git
 set ASYN=%SUPPORT%\asyn-4-23
 set EPICS_DISPLAY_PATH=%AREA_DETECTOR%\ADPICam\PICamApp\op\adl;%AREA_DETECTOR%\ADCore\ADApp\op\adl;%ASYN%\opi\medm
 echo %EPICS_DISPLAY_PATH%
-start medm -x -macro "P=13PICAM1:, R=cam1:" PICam.adl PICAMBase.adl PICAM_Readout.adl
+start medm -x -macro "P=13PICAM1:, R=cam1:" PICAMBase.adl
