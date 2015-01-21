371ab68166f38fb6c91d6db346709bc25c3e0b59
John Hammond <JPHammonds@anl.gov>
2015-01-21 10:36:06 -0600

    Create Generic PICAM base display.

--- iocs/PICamIOC/iocBoot/iocPICam/start_epics-displays.bat
+++ iocs/PICamIOC/iocBoot/iocPICam/start_epics-displays.bat
@@ -3,7 +3,4 @@ set AREA_DETECTOR=%SUPPORT%\areaDetector_2_git
 set ASYN=%SUPPORT%\asyn-4-23
 set EPICS_DISPLAY_PATH=%AREA_DETECTOR%\ADPICam\PICamApp\op\adl;%AREA_DETECTOR%\ADCore\ADApp\op\adl;%ASYN%\opi\medm
 echo %EPICS_DISPLAY_PATH%
-start medm -x -macro "P=13PICAM1:, R=cam1:" PICam.adl ADBase.adl PICAM_Readout.adl
-REM start medm -x -macro "P=13PICAM1:, R=cam1:" ADBase.adl
-REM ..\..\bin\windows-x64-debug\PICamApp st.cmd
-
+start medm -x -macro "P=13PICAM1:, R=cam1:" PICam.adl PICAMBase.adl PICAM_Readout.adl
