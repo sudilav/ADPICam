8d8148f873c4416937b5b534f36563025660fbf9
John Hammond <JPHammonds@anl.gov>
2015-01-27 13:11:18 -0600

    Change location for areadetector files.

--- iocs/PICamIOC/iocBoot/iocPICam/start_epics-displays.bat
+++ iocs/PICamIOC/iocBoot/iocPICam/start_epics-displays.bat
@@ -1,5 +1,5 @@
-set SUPPORT=C:\PICAM\synApps_5_7\support
-set AREA_DETECTOR=%SUPPORT%\areaDetector_2_git
+set SUPPORT=C:\jhSrc\synApps_5_7\support
+set AREA_DETECTOR=%SUPPORT%\areaDetector-2-x-git
 set ASYN=%SUPPORT%\asyn-4-23
 set EPICS_DISPLAY_PATH=%AREA_DETECTOR%\ADPICam\PICamApp\op\adl;%AREA_DETECTOR%\ADCore\ADApp\op\adl;%ASYN%\opi\medm
 echo %EPICS_DISPLAY_PATH%
